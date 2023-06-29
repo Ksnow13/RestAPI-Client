86008a3271b5077a9916d56271016fbc7a2efb00
Ksnow <107448069+Ksnow13@users.noreply.github.com>
2023-06-29 14:05:16 -0230

    added airport report test

--- src/test/java/com/keyin/HTTPRestCLIApplicationTest.java
+++ src/test/java/com/keyin/HTTPRestCLIApplicationTest.java
@@ -5,6 +5,7 @@ import com.keyin.domain.Airport;
 import com.keyin.http.cli.HTTPRestCLIApplication;
 import com.keyin.http.client.RESTClient;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -21,7 +22,8 @@ public class HTTPRestCLIApplicationTest {
     private RESTClient mockRESTClient;
 
     @Test
-    public void testGenerateAirportReport(){
+    @DisplayName("Test Aircraft Report")
+    public void testGenerateAircraftReport(){
         HTTPRestCLIApplication httpRestCLIApplicationUnderTest = new HTTPRestCLIApplication();
 
         Airport airport1 = new Airport();
@@ -36,23 +38,12 @@ public class HTTPRestCLIApplicationTest {
         airport2.setAreaCode("M0X S0Q");
         airport2.setCityId(2);
 
-        Airport airport3 = new Airport();
-        airport3.setId(2);
-        airport3.setName("Calgary Airport");
-        airport3.setAreaCode("J0X R0Q");
-        airport3.setCityId(2);
-
         List<Airport> landTakeOffList1 = new ArrayList<>();
         landTakeOffList1.add(airport1);
         landTakeOffList1.add(airport2);
-        landTakeOffList1.add(airport3);
 
         List<Airport> landTakeOffList2 = new ArrayList<>();
-        landTakeOffList1.add(airport2);
-        landTakeOffList1.add(airport3);
-
-        List<Airport> landTakeOffList3 = new ArrayList<>();
-        landTakeOffList3.add(airport1);
+        landTakeOffList1.add(airport1);
 
         Aircraft aircraft1 = new Aircraft();
         aircraft1.setId(1);
@@ -68,23 +59,47 @@ public class HTTPRestCLIApplicationTest {
         aircraft2.setNumberOfPassengers(300);
         aircraft2.setAllowedAirportList(landTakeOffList2);
 
-        Aircraft aircraft3 = new Aircraft();
-        aircraft3.setId(3);
-        aircraft3.setType("Blue flyer");
-        aircraft3.setAirlineName("private owned");
-        aircraft3.setNumberOfPassengers(6);
-        aircraft3.setAllowedAirportList(landTakeOffList3);
-
-
         List<Aircraft> aircraftList = new ArrayList<>();
         aircraftList.add(aircraft1);
         aircraftList.add(aircraft2);
-        aircraftList.add(aircraft3);
 
         Mockito.when(mockRESTClient.getAllAircraft()).thenReturn(aircraftList);
         httpRestCLIApplicationUnderTest.setRestClient(mockRESTClient);
 
         Assertions.assertTrue(httpRestCLIApplicationUnderTest.generateAircraftReport().contains("Boeing 747"));
+        Assertions.assertTrue(httpRestCLIApplicationUnderTest.generateAircraftReport().contains("Airbus 767"));
+        Assertions.assertFalse(httpRestCLIApplicationUnderTest.generateAircraftReport().contains("Newfoundland"));
     }
+
+    @Test
+    @DisplayName("Test Airport Report")
+    public void testGenerateAirportReport(){
+        HTTPRestCLIApplication httpRestCLIApplicationUnderTest = new HTTPRestCLIApplication();
+
+        Airport airport1 = new Airport();
+        airport1.setId(1);
+        airport1.setName("St. John's Airport");
+        airport1.setAreaCode("A0A Z0A");
+        airport1.setCityId(1);
+
+        Airport airport2 = new Airport();
+        airport2.setId(2);
+        airport2.setName("Edmonton Airport");
+        airport2.setAreaCode("M0X S0Q");
+        airport2.setCityId(2);
+
+        List<Airport> airportList = new ArrayList<Airport>();
+        airportList.add(airport1);
+        airportList.add(airport2);
+
+        Mockito.when(mockRESTClient.getAllAirport()).thenReturn(airportList);
+        httpRestCLIApplicationUnderTest.setRestClient(mockRESTClient);
+
+        Assertions.assertTrue(httpRestCLIApplicationUnderTest.generateAirportReport().contains("M0X S0Q"));
+        Assertions.assertFalse(httpRestCLIApplicationUnderTest.generateAirportReport().contains("random airport name"));
+
+    }
+
+
 
 }
